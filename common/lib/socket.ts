814517297e2d2310c43508ba17c11dd4c8f48182
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2024-11-16 20:04:50 +0530

    Update socket.ts

--- common/lib/socket.ts
+++ common/lib/socket.ts
@@ -1,4 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL);
+//const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
+//export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL);
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
